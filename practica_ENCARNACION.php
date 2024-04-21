302ffb633b991e9ff4f5d0f5aa8a80653e3aa0b2
tecnotrayamar2 <gooracle@gmail.com>
2024-04-21 12:01:45 +0200

    Almacenando nuestro proyecto en el repositorio Github como práctica

---
+++ practica_ENCARNACION.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Script PHP para la práctica de documentación.
+ *
+ * Este script contiene dos funciones documentadas.
+ *
+ * @package PracticaDocumentacion
+ * @author Tu Nombre
+ * @version 1.0
+ * @internal Este comentario es solo para desarrolladores.
+ */
+
+/**
+ * Función que suma dos números.
+ *
+ * Esta función suma dos números y devuelve el resultado.
+ *
+ * @param int $num1 El primer número.
+ * @param int $num2 El segundo número.
+ * @return int El resultado de la suma.
+ */
+function suma($num1, $num2) {
+    return $num1 + $num2;
+}
+
+/**
+ * Función que multiplica dos números.
+ *
+ * Esta función multiplica dos números y devuelve el resultado.
+ *
+ * @param int $num1 El primer número.
+ * @param int $num2 El segundo número.
+ * @return int El resultado de la multiplicación.
+ */
+function multiplicacion($num1, $num2) {
+    return $num1 * $num2;
+}
+?>
